1bf165ba8983fafb378328499ee7794d552f1f81
Lynda Santiago <lyntechi@gmail.com>
2020-09-17 16:57:34 -0400

    created knexfile and db config file

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const welcomeRouter = require("../welcome/welcome-router");
+const authRouter = require("../auth/auth-router");
 const helmet = require("helmet");
 const express = require("express");
 const server = express();
 
 server.use(helmet());
+server.use(express.json());
 server.use("/", welcomeRouter);
-
+server.use("/api/auth")
 module.exports = server;
